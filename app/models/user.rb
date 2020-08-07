c94c4f69783568c3fd8f36edfe3b19e36343787a
kyota-22 <kyota.bb@gmail.com>
2020-08-07 10:41:49 +0900

    second_commmit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 # # ここから追記
-#   has_many :products dependent: :destroy
-#   has_one :delivery_destination dependent: :destroy
-#   has_one :credit_card dependent: :destroy
+#   has_many :products, dependent: :destroy
+#   has_one :delivery_destination, dependent: :destroy
+#   has_one :credit_card, dependent: :destroy
 
-#   validates :nickname, presence: true, uniqueness: true
-#   validates :family_name, presence: true
-#   validates :first_name, presence: true
-#   validates :family_name_kana, presence: true
-#   validates :first_name_kana, presence: true
-#   validates :birth_day, presence: true
+  validates :nickname, presence: true
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_day, presence: true
 end
