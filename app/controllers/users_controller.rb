8bcf9dd6f8544e12b8f96430bd37b66713fb56fa
kyota-22 <kyota.bb@gmail.com>
2020-08-08 14:08:07 +0900

    複数モデルへデータを送る記述を追加、Rスペックの導入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+    @user.build_address
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to root_path
+  end
+
   def edit
   end
 
@@ -14,7 +25,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:nickname, :user_image, :profile, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day)
+    params.require(:user).permit(:nickname, :user_image, :profile, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day, delivery_destination_attributes:[:id, :post_code, :prefecture, :city, :address, :building_name, :phone_number])
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 # # ここから追記
 #   has_many :products, dependent: :destroy
-#   has_one :delivery_destination, dependent: :destroy
+  has_one :delivery_destination, dependent: :destroy
 #   has_one :credit_card, dependent: :destroy
+# 複数モデルのデータ送信↓
+  # accepts_nested_attributes_for :delivery_destinations
 
   validates :nickname, presence: true, length: { maximum: 15 }
   validates :family_name, presence: true, length: { maximum: 15 }
