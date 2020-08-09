cfea4b15f057dce40736e4d151d39fc3627b1bb4
kyota-22 <67886161+kyota-22@users.noreply.github.com>
2020-08-09 16:00:49 +0900

    Revert "Revert "Feature 0806 kaneshima devise""

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
   private
 
   def basic_auth
@@ -14,5 +15,8 @@ class ApplicationController < ActionController::Base
     Rails.env.production?
   end
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :user_image, :profile, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day])
+  end
   
 end
