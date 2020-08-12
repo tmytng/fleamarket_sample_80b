e3f0c09eb622242349f967b9590c5c4317d8efeb
kyota-22 <kyota.bb@gmail.com>
2020-08-12 11:11:06 +0900

    クレジットカードテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_11_015249) do
+ActiveRecord::Schema.define(version: 2020_08_12_015713) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -20,9 +20,18 @@ ActiveRecord::Schema.define(version: 2020_08_11_015249) do
     t.index ["name", "ancestry"], name: "index_categories_on_name_and_ancestry"
   end
 
+  create_table "credit_cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_credit_cards_on_user_id"
+  end
+
   create_table "delivery_destinations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.string "family_name", null: false
+    t.string "user", null: false
     t.string "first_name", null: false
     t.string "family_name_kana", null: false
     t.string "first_name_kana", null: false
@@ -58,5 +67,6 @@ ActiveRecord::Schema.define(version: 2020_08_11_015249) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "credit_cards", "users"
   add_foreign_key "delivery_destinations", "users"
 end
