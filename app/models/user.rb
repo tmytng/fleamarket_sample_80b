0df6c1d56a494acb150ac3b434330bb772b411aa
kyota-22 <kyota.bb@gmail.com>
2020-08-12 13:16:34 +0900

    ユーザーモデルのアソシエーションを追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 # # ここから追記
 #   has_many :products, dependent: :destroy
   has_one :delivery_destination, dependent: :destroy
-#   has_one :credit_card, dependent: :destroy
+  has_one :credit_card, dependent: :destroy
 
 # バリデーションの追加
   validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day, :password, :password_confirmation, :email, presence: true
