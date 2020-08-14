1f236dc844793d4af630d9b737ed60cd595d75ac
rikuan <kuriudona2812@gmail.com>
2020-08-14 14:20:49 +0900

    ビューの作成中

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,26 +1,27 @@
 class ProductsController < ApplicationController
     def index
         @products = Product.all.order("id DESC").limit(10)
+        @products = Product.includes(:images).order('created_at DESC')
     end
 
     def new
         @product = Product.new
-        @product.images.build()
+        # @product.images.build()
         @parent = Category.where(id: 1..13)
     end
 
     def create
         @product = Product.new(product_params)
 
-        if @product.save
+        # if @product.save
             # params[:product_images][:image].each do |image|
             #     @product.photos.create(image: image, product_id: @product.id)
             # 途中なのであしからず
-            end
-            redirect_to root_path
-        else
-            redirect_to new_product_path
-        end
+        #     end
+        #     redirect_to root_path
+        # else
+        #     redirect_to new_product_path
+        # end
     end
 
     def product_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # デバイス用
   devise_for :users
   root 'items#index'
+  resources :products, only:[:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
