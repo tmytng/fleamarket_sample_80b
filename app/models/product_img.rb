fcea7a7cbd80e33362f89b10dd7de42d2c351c23
rikuan <67988609+rikuan@users.noreply.github.com>
2020-08-14 14:24:54 +0900

    Feature ando 0812 create product imgs (#52)
    * create table img
    
    * ビューの作成中

---
+++ app/models/product_img.rb
@@ -0,0 +1,4 @@
+class ProductImg < ApplicationRecord
+    belongs_to :product, optional: true
+    mount_uploader :image, ImageUploader
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,11 @@ Rails.application.routes.draw do
   # デバイス用
   devise_for :users
   root 'items#index'
-  
-  resources :items
-  resources :products, only: [:show]
-  
+  resources :products, only:[:new, :create, :show]
+  resources :items  
   resources :credit_cards, only: [:new, :create, :show] do
     collection do
       delete 'delete', to: 'credit_cards#delete'
     end
   end
-
 end
