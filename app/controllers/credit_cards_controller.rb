0d1ccbbf83db5a7529076a047574554b96ecf518
kyota-22 <kyota.bb@gmail.com>
2020-08-16 14:32:42 +0900

    購入機能の実装

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -9,7 +9,7 @@ class CreditCardsController < ApplicationController
   end
 
   def create #payjpとCardのデータベース作成を実施します。
-    Payjp.api_key = "PAYJP_PRIVATE_KEY"
+    Payjp.api_key = "秘密鍵"
     if params['payjp-token'].blank?
       redirect_to action: "new"
     else
@@ -25,26 +25,58 @@ class CreditCardsController < ApplicationController
       end
     end
   end
-
-  def pay
-    product = Product.find(credit_card_params[:product_id])
+  
+  def show
+    # @product = Product.find(params[:id])
     card = CreditCard.where(user_id: current_user.id).first
-    Payjp.api_key = 'PAYJP_PRIVATE_KEY'
-    Payjp::Charge.create(
-    amount:  product.price,
-    customer: card.customer_id,
-    currency: 'jpy',
-    )
-    product[:status] = 1
-    product.save
-   redirect_to action: 'complete'
+    #Cardテーブルは前回記事で作成、テーブルからpayjpの顧客IDを検索
+    if card.blank?
+      #登録された情報がない場合にカード登録画面に移動
+      redirect_to action: "new"
+    else
+      Payjp.api_key = "秘密鍵"
+      #保管した顧客IDでpayjpから情報取得
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
+      @default_card_information = customer.cards.retrieve(card.card_id)
+    end
   end
 
+  def pay
+    @product = Product.find(params[:id])
+    card = CreditCard.where(user_id: current_user.id).first
+    Payjp.api_key = '秘密鍵'
+    # Payjp::Charge.create(
+    # # amount:  product.price,
+    # amount: 13500
+    # # customer: card.customer_id,
+    # # currency: 'jpy',
+    # )
+    # product[:trading_status] = 1
+    # product.save
+  redirect_to action: 'done'
+  end
+
+  def done
+    # @product = Product.find(params[:id])
+  end
+
+  def index 
+    card = CreditCard.where(user_id: current_user.id).first
+    if card.blank?
+      redirect_to action: "new" 
+    else
+      Payjp.api_key = "秘密鍵"
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @default_card_information = customer.cards.retrieve(card.card_id)
+    end
+  end
+  
   def delete #PayjpとCardデータベースを削除します
     card = CreditCard.where(user_id: current_user.id).first
     if card.blank?
     else
-      Payjp.api_key = "PAYJP_PRIVATE_KEY"
+      Payjp.api_key = "秘密鍵"
       customer = Payjp::Customer.retrieve(card.customer_id)
       customer.delete
       card.delete
@@ -52,14 +84,9 @@ class CreditCardsController < ApplicationController
       redirect_to action: "new"
   end
 
-  def show #Cardのデータpayjpに送り情報を取り出します（キータのindexも兼ねてる,参照のconfimation）
-    card = CreditCard.where(user_id: current_user.id).first
-    if card.blank?
-      redirect_to action: "new" 
-    else
-      Payjp.api_key = "PAYJP_PRIVATE_KEY"
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card_information = customer.cards.retrieve(card.card_id)
-    end
+  private
+  def credit_card_params
+    params.permit('payjp-token',:product_id)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,18 @@ Rails.application.routes.draw do
   # デバイス用
   devise_for :users
   root 'items#index'
-  resources :products, only:[:new, :create, :show]
-  resources :items  
-  resources :credit_cards, only: [:new, :create, :show] do
-    collection do
-      delete 'delete', to: 'credit_cards#delete'
+  resources :products, only:[:new, :create, :show] do
+    resources :credit_cards, only:[:new, :create, :show, :index] do
+      collection do
+        delete 'delete', to: 'credit_cards#delete'
+        get 'done', to: 'credit_cards#done'
+      end
+
+      member do
+        post 'pay', to: 'credit_cards#pay'
+      end
     end
   end
+  resources :items  
+
 end
