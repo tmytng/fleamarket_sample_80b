b5be0afea1018d9fcf287500799f4c9297c48f9e
tmytng <tmytng.2020@gmail.com>
2020-08-17 21:23:07 +0900

    サーバサイド部分作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_08_13_001904) do
-
+ActiveRecord::Schema.define(version: 2020_08_17_071616) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
