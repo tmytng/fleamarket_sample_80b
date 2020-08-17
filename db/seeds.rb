e4f4b4446fab463ba0992884b09733ecfa3f3fa6
kyota-22 <kyota.bb@gmail.com>
2020-08-17 18:55:31 +0900

    gitignore確認

--- db/seeds.rb
+++ db/seeds.rb
@@ -355,70 +355,70 @@
 
 # ダミーデータ（ユーザー）
 
-# p "create Users"
-# User.create!(
-#   email: 'user1@test.com',
-#   password: 'password',
-#   password_confirmation: 'password',
-#   nickname: 'tarow',
-#   profile: 'テスト用ユーザーアカウント',
-#   family_name: '新宿',
-#   first_name: '太郎',
-#   family_name_kana: 'しんじゅく',
-#   first_name_kana: 'たろう',
-#   birth_day: '2020-08-01'
-#   )
-#   User.create!(
-#   email: 'user2@test.com',
-#   password: 'password',
-#   password_confirmation: 'password',
-#   nickname: 'tarow2',
-#   profile: 'テスト用ユーザーアカウント2',
-#   family_name: '渋谷',
-#   first_name: '太郎',
-#   family_name_kana: 'シブヤ',
-#   first_name_kana: 'タロ',
-#   birth_day: '2020-07-01'
-#   )
-#   User.create!(
-#   email: 'user3@test.com',
-#   password: '12345678',
-#   password_confirmation: '12345678',
-#   nickname: 'testman',
-#   profile: 'テスト用ユーザーアカウント3',
-#   family_name: '池袋',
-#   first_name: '太郎',
-#   family_name_kana: 'イケブクロ',
-#   first_name_kana: 'タロ',
-#   birth_day: '2020-06-01'
-#   )
+p "create Users"
+User.create!(
+  email: 'user1@test.com',
+  password: 'password',
+  password_confirmation: 'password',
+  nickname: 'tarow',
+  profile: 'テスト用ユーザーアカウント',
+  family_name: '新宿',
+  first_name: '太郎',
+  family_name_kana: 'しんじゅく',
+  first_name_kana: 'たろう',
+  birth_day: '2020-08-01'
+  )
+  User.create!(
+  email: 'user2@test.com',
+  password: 'password',
+  password_confirmation: 'password',
+  nickname: 'tarow2',
+  profile: 'テスト用ユーザーアカウント2',
+  family_name: '渋谷',
+  first_name: '太郎',
+  family_name_kana: 'シブヤ',
+  first_name_kana: 'タロ',
+  birth_day: '2020-07-01'
+  )
+  User.create!(
+  email: 'user3@test.com',
+  password: '12345678',
+  password_confirmation: '12345678',
+  nickname: 'testman',
+  profile: 'テスト用ユーザーアカウント3',
+  family_name: '池袋',
+  first_name: '太郎',
+  family_name_kana: 'イケブクロ',
+  first_name_kana: 'タロ',
+  birth_day: '2020-06-01'
+  )
 
 # ダミーデータ（プロダクト）
 
-  # p "Create Products"
-  # Product.create!(
-  #   name: 'テスト商品1',
-  #   price: '5000',
-  #   description: 'テスト登録です。',
-  #   condition: 1,
-  #   size: 'L',
-  #   shipping_cost: 1,
-  #   shipping_days: 1,
-  #   prefecture: '東京都',
-  #   trading_status: '出品中',
-  #   brand_id: nil,
-  #   user_id: 1
-  #   )
-  # Product.create!(
-  #   name: 'テスト商品2',
-  #   price: '100000',
-  #   description: 'テスト登録2です。',
-  #   condition: 2,
-  #   size: 'S',
-  #   shipping_cost: 2,
-  #   shipping_days: 2,
-  #   prefecture: '神奈川県',
-  #   trading_status: '出品中',
-  #   brand_id: nil,
-  #   user_id: 1
-  #   )
+  p "Create Products"
+  Product.create!(
+    name: 'テスト商品1',
+    price: '5000',
+    description: 'テスト登録です。',
+    condition: 1,
+    size: 'L',
+    shipping_cost: 1,
+    shipping_days: 1,
+    prefecture: '東京都',
+    trading_status: '出品中',
+    brand_id: nil,
+    user_id: 1
+    )
+  Product.create!(
+    name: 'テスト商品2',
+    price: '100000',
+    description: 'テスト登録2です。',
+    condition: 2,
+    size: 'S',
+    shipping_cost: 2,
+    shipping_days: 2,
+    prefecture: '神奈川県',
+    trading_status: '出品中',
+    brand_id: nil,
+    user_id: 1
+    )
