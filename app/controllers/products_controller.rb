3ffc64fc234c0d99ef5ccd3848a0e11093f06aab
rikuan <kuriudona2812@gmail.com>
2020-08-17 10:14:39 +0900

    マークアップ実装途中

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,13 +6,12 @@ class ProductsController < ApplicationController
 
     def new
         @product = Product.new
-        # @product.images.build()
+        # @product.images.build
         @parent = Category.where(id: 1..13)
     end
 
     def create
         @product = Product.new(product_params)
-
         # if @product.save
             # params[:product_images][:image].each do |image|
             #     @product.photos.create(image: image, product_id: @product.id)
@@ -29,6 +28,25 @@ class ProductsController < ApplicationController
 
     def product_params
         params.require(:product).permit(:name, :price, :description, :condition, :size, :shipping_cost, :shipping_days, :prefecture, :trading_status)
+    end
+
+    def set_category
+        @grandchild = Category.find("#{@product.category_id}")
+        @child = @grandchild.parent
+        @parent = @child.parent
+    end
+
+    def set_current_category_group
+        @selected_grandson_category = Category.find(@product.category_id)
+        @selected_child_category = @selected_grandson_category.parent
+        @selected_parent_category = @selected_child_category.parent
+        @grandchild_group = @selected_grandson_category.siblings
+        @child_group = @selected_child_category.siblings
+    end
+
+    def set_image
+        @image = @product.images.first
+        @images = @product.images
     end
 
 end
