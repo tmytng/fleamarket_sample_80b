c792acb2aec0f0c59e46bbab0be7c8df9817bf9b
tmytng <tmytng.2020@gmail.com>
2020-08-20 00:57:45 +0900

    [修正]product_showの画像表示

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,7 +11,6 @@ CarrierWave.configure do |config|
     aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
     region: 'ap-northeast-1'
-    path_style: true
   }
 
 
