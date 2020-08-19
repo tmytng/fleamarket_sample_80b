cec952cffc4d9a880939547e2c9c627401f13b5a
tmytng <tmytng.2020@gmail.com>
2020-08-19 21:50:32 +0900

    S3へ画像アップロード先を変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,8 +5,8 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
 
-  storage :file
-  # storage :fog
+  # storage :file
+  storage :fog
 
 
   # Override the directory where uploaded files will be stored.
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,13 +5,15 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_provider = 'fog/aws'
+  config.fog_directory  = 'fleamarket80b'
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
     region: 'ap-northeast-1'
+    path_style: true
   }
 
-  config.fog_directory  = 'fleamarket80b'
+
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/fleamarket80b'
 end
