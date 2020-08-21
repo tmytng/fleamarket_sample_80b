80b3dc5655dd1d26882e39dc09edfa39fe7dd0ed
kyota-22 <kyota.bb@gmail.com>
2020-08-21 20:30:35 +0900

    配送先住所登録フォームの作成

--- app/controllers/delivery_destinations_controller.rb
+++ app/controllers/delivery_destinations_controller.rb
@@ -1,10 +1,10 @@
 class DeliveryDestinationsController < ApplicationController
     def new
-        @delivery_destination = Delivery_destination.new
+        @delivery_destination = DeliveryDestination.new
     end
 
     def create
-        @delivery_destination = Delivery_destination.new(delivery_destination_params)
+        @delivery_destination = DeliveryDestination.new(delivery_destination_params)
         if @delivery_destination.save
             redirect_to root_path
         else
@@ -15,7 +15,7 @@ class DeliveryDestinationsController < ApplicationController
     private
 
     def delivery_destination_params
-        params.require(:delivery_destination).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :post_code, :prefecture, :city, :address, :phone_number)
+        params.require(:delivery_destination).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :post_code, :prefecture, :city, :address, :buiding_name, :phone_number).merge(user_id: current_user.id)
         # 複数テーブルの情報を一つのフォームから送る時↓
         # params.require(:user).permit(:nickname, :user_image, :profile, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day, delivery_destination_attributes:[:post_code, :prefecture, :city, :address, :building_name, :phone_number])
     end
