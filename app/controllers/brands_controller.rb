39293d0cb26dec3159e80a53c4b1dfecbc395825
shunta-ux <ten3maewomiro1515maemuking25@gmail.com>
2020-08-21 20:31:56 +0900

    売り切れ表示実装前

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,5 +1,2 @@
 class BrandsController < ApplicationController
 end
-
-def show
-end
--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,5 @@ Rails.application.routes.draw do
     end
   end
   resources :users, only: :show
-  resources :brands, only: :show
 end
 
