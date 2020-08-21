fdbd7c53b55d10b63cae6ed6f4c71256339d879c
tmytng <tmytng.2020@gmail.com>
2020-08-21 10:44:16 +0900

    編集画面の修正

---
+++ app/assets/javascripts/modules/product_edit.js
@@ -0,0 +1,13 @@
+// $(function () {
+//   $('#edit_btn').on("submit", function (e) {
+//     e.preventDefault()
+//     console.log("hoge")
+//   })
+// })
+
+
+
+//   // $('#edit_btn').click(function () {
+//   //   return false;
+//   //   $('.form-field--cat').removeClass("displayNone");
+//   // })
