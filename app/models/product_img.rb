ac49c48f5de99282edc4cd33071da1644ee99be7
tmytng <tmytng.2020@gmail.com>
2020-08-27 15:08:51 +0900

    [修正]product_editのカテゴリー未選択制御

--- app/models/product_img.rb
+++ app/models/product_img.rb
@@ -1,5 +1,5 @@
 class ProductImg < ApplicationRecord
     belongs_to :product, optional: true
     mount_uploader :image, ImageUploader
-    validates :image, presence: true
+    # validates :image, presence: true
 end
